365373790cf940adce33591c99fac84e313d276e
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2019-11-04 18:20:01 -0800

    Added Bridging header

---
+++ ValidateEarlGrey/ValidateEarlGreyUITests/ValidateEarlGreySwiftBridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  Header.h
+//  ValidateEarlGreyUITests
+//
+//  Created by Venkatnarayansetty, Badarinath on 11/4/19.
+//  Copyright © 2019 Venkatnarayansetty, Badarinath. All rights reserved.
+//
+
+#import <EarlGreyTest/EarlGrey.h>
